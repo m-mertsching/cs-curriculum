af78be066421690dbd265b995307bce9d28e367c
m-mertsching <mmertsching6430@student.pps.net>
2024-11-18 15:21:39 -0800

    added jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     public float ySpeed;
     private float _yVector;
     private float _yDirection;
+    private Collider2D collider2D;
+    public float length;
 
     private Rigidbody2D rb;
     //
@@ -18,14 +20,25 @@ public class PlayerController : MonoBehaviour
     private int health;
     
     public bool overworld; 
+    public float jumpForce = 5f;  
+    private bool isGrounded;       
+    public LayerMask groundLayer;
 
     private void Start()
     {
-        // rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
 
+        xSpeed = 5f;
+        _xVector = 0f;
+        _xDirection = 0;
+
+        ySpeed = 5f;
+        _yVector = 0f;
+        _yDirection = 0f;
+        
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
-        
+        collider2D = GetComponent<Collider2D>();
         
         if (overworld)
         {
@@ -38,13 +51,6 @@ public class PlayerController : MonoBehaviour
         }
 
 
-        xSpeed = 5f;
-        _xVector = 0f;
-        _xDirection = 0;
-
-        ySpeed = 5f;
-        _yVector = 0f;
-        _yDirection = 0f;
     }
 
     private void Update()
@@ -58,15 +64,38 @@ public class PlayerController : MonoBehaviour
         _yVector = ySpeed * _yDirection * Time.deltaTime;
 
         transform.Translate(0, _yVector, 0);
-
+        isGrounded = CheckIfGrounded();
         
+        
+        if (isGrounded && Input.GetKeyDown("space"))
+        {
+            Jump();
+        }
+
+        //if (Input.)
+        //RaycastHit2D leftray = Physics2D.Raycast(new Vector2(transform.position.x - collider2D.bounds.x, transform.position.y))
+        //RaycastHit2D rightray = Physics2D.Raycast(new Vector2(transform.position.x + collider2D.bounds.x, transform.position.y))    
 
 
 
     }
 
     //for organization, put other built-in Unity functions here
-   
+    private bool CheckIfGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, collider2D.bounds.extents.y + 0.1f, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
