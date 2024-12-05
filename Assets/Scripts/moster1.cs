ec21410ddceb004ddad2f6e6dc89d0bdaf2ef5c2
m-mertsching <mmertsching6430@student.pps.net>
2024-12-05 15:35:58 -0800

    not sure

--- Assets/Scripts/moster1.cs
+++ Assets/Scripts/moster1.cs
@@ -6,7 +6,7 @@ public class moster1 : MonoBehaviour
     public float speed = 2f; // Speed of the enemy
     public float detectionRange = 5f; // Distance at which the enemy detects the player
     public GameObject player; // Reference to the player's transform
-
+    private bool movingForward = true;
     private int currentPatrolIndex = 0;
     private bool isChasing = false;
     private float distanceToPlayer;
@@ -29,7 +29,11 @@ public class moster1 : MonoBehaviour
     private void Start()
     { 
         SwitchState(States.patrol);
-    
+        if (patrolPoints.Length > 0)
+        {
+            // Start patrolling if patrol points are set
+            currentPatrolIndex = 0;
+        }
         
     }
 
@@ -63,17 +67,61 @@ public class moster1 : MonoBehaviour
         }
     }
     
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            if(state == States.patrol)
+                SwitchState(States.attack);
+            else
+            {
+                SwitchState(States.patrol);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            if (state == States.attack)
+            {
+                SwitchState(States.patrol);
+            }
+        }
+    }
+    
     void Patrol()
     {
         if (patrolPoints.Length == 0) return;
-        
+        currentPatrolIndex = 1;
         Transform targetPoint = patrolPoints[currentPatrolIndex];
+        print("heading towared" + currentPatrolIndex);
         
         transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
         
         if (Vector3.Distance(transform.position, targetPoint.position) < 0.1f)
         {
-            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+            print("switch direction");
+            //Debug.Log($"Current Patrol Index: {currentPatrolIndex}, Moving Forward: {movingForward}");
+            
+            if (movingForward)
+            {
+               
+                    movingForward = false;
+                    currentPatrolIndex = 1;
+            }
+            
+            else
+            {
+               
+                    currentPatrolIndex = 0;
+                    movingForward = true;
+            }
+            
+            //currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
         }
     }
     
--- Assets/Scripts/moster1animator.cs
+++ Assets/Scripts/moster1animator.cs
@@ -6,10 +6,56 @@ public class moster1animator : MonoBehaviour
 
     Vector3 prevPos;
     Animator anim;
-    
+  
+
+    // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 movement = transform.position - prevPos;
+
+        if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+        {
+            if (movement.x > 0f)
+            {
+                anim.SetInteger("Direction", 0);
+            }
+            if (movement.x < 0f)
+            {
+                anim.SetInteger("Direction", 2);
+            }
+        }
+        /*else
+        {
+            if (movement.y > 0f)
+            {
+                anim.SetInteger("Direction", 1);
+            }
+            if (movement.y < 0f)
+            {
+                anim.SetInteger("Direction", 3);
+            }
+        }*/
+
+        prevPos = transform.position;
+
+        /*if (Input.GetMouseButton(0))
+        {
+            Attack();
+        }*/
+
+        IsAttacking = anim.GetCurrentAnimatorStateInfo(0).IsTag("Attack");
+    }
+
+    // Call this function from another script for the orc to attack!
+    public void Attack()
+    {
+        anim.SetTrigger("Attack");
     }
 }
